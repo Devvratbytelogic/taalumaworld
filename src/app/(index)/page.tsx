6a4460aeba2d592bdb0b50e5076e048bff4b145b
Devvratbytelogic <142411170+Devvratbytelogic@users.noreply.github.com>
2026-02-09 14:50:26 +0530

    Add PrimaryHeader and PrimaryFooter components

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/ui/Button'
 export default function HomePage() {
   return (
     <>
-      <h1 className='font-ubuntu'>Home</h1>
+      <h1 className='font-ubuntu'>Home Page</h1>
       <Button>Click me</Button>
     </>
   )

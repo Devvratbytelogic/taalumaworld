496b412cff9c0b18409ca04cf16a3b634c624599
Devvratbytelogic <142411170+Devvratbytelogic@users.noreply.github.com>
2026-02-20 09:12:21 +0530

    update 	modified:   src/app/admin/page.tsx 	modified:   src/styles/globals.css

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -270,11 +270,13 @@ export default function AdminPanelRoutePage() {
                         {/* Search Bar - Desktop */}
                         <div className="hidden lg:flex flex-1 max-w-xl">
                             <div className="relative w-full">
-                                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+                                {/* <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-muted-foreground" /> */}
                                 <Input
+                                    startContent={<Search className="h-5 w-5 text-muted-foreground" />}
                                     type="search"
                                     placeholder="Search users, books, chapters..."
                                     value={searchQuery}
+                                    radius='full'
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
                                     className="pl-10 pr-4 w-full rounded-full"
                                 />

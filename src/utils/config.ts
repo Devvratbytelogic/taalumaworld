973b5b3dc98f718205aa12aebb805f3b737f9f43
Devvratbytelogic <support@bytelogicindia.com>
2026-05-13 08:20:23 +0530

    update API base URL in config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,3 @@
 export const GOOGLE_API_KEY = "AIzaSyBYTVr8YYdf7qm_TrZdTBKV07S-apoYEbs"
 
-export const API_BASE_URL = "https://taalumaapi.bytelogictech.com/api/"
-// export const API_BASE_URL = "http://192.168.1.30:3200/api"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://taalumaapi.bytelogictech.com/api/"

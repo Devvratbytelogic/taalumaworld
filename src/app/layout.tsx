363a2131deeb412cbb84b35b960ef93e5e4c963a
Devvratbytelogic <support@bytelogicindia.com>
2026-05-28 10:06:21 +0530

    update global settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { AppProviders } from "../components/providers/AppProviders";
 import ConditionalSiteLayout from "@/components/layout/ConditionalSiteLayout";
 import { ContentProtection } from "@/components/ContentProtection";
 import { API_BASE_URL } from "@/utils/config";
+import Script from "next/script";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -18,12 +19,20 @@ const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
 });
 
-export async function generateMetadata(): Promise<Metadata> {
+async function fetchGlobalSettings() {
   try {
     const res = await fetch(`${API_BASE_URL}user/get-global`, { next: { revalidate: 3600 } });
     const json = await res.json();
-    const data = json?.data;
+    return json?.data ?? null;
+  } catch {
+    return null;
+  }
+}
 
+export async function generateMetadata(): Promise<Metadata> {
+  const data = await fetchGlobalSettings();
+
+  if (data) {
     return {
       title: data?.meta_title || data?.marketplace_name || data?.platformName || 'TaalumaWorld',
       description: data?.meta_description || data?.platformDescription || '',
@@ -33,21 +42,52 @@ export async function generateMetadata(): Promise<Metadata> {
         description: data?.meta_description || data?.platformDescription || '',
       },
     };
-  } catch {
-    return {
-      title: 'TaalumaWorld',
-      description: '',
-    };
   }
+
+  return {
+    title: 'TaalumaWorld',
+    description: '',
+  };
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const globalSettings = await fetchGlobalSettings();
+
   return (
     <html lang="en" className={`${roboto.variable} ${ubuntu.variable}`}>
+      <head>
+        {/* Google Analytics */}
+        {globalSettings?.google_analytics_id && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${globalSettings?.google_analytics_id}`}
+              strategy="beforeInteractive"
+            />
+            <Script id="ga-init" strategy="beforeInteractive">
+              {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${globalSettings?.google_analytics_id}');`}
+            </Script>
+          </>
+        )}
+
+        {/* Google Tag Manager */}
+        {globalSettings?.google_tag_manager && (
+          <Script id="gtm-init" strategy="beforeInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${globalSettings?.google_tag_manager}');`}
+          </Script>
+        )}
+
+        {/* Schema Markup / JSON-LD */}
+        {globalSettings?.schema_markup && (
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: globalSettings?.schema_markup }}
+          />
+        )}
+      </head>
       <body className="antialiased">
         <ContentProtection />
         <AppProviders>

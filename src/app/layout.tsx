ef39472d970cd7a42182073767621b354741aab5
Devvratbytelogic <support@bytelogicindia.com>
2026-07-28 11:03:44 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,28 +75,8 @@ export default async function RootLayout({
 
 
   return (
-    <html lang="en" className={`${roboto.variable} ${ubuntu.variable}`}>
+    <html lang="en" className={`${roboto.variable} ${ubuntu.variable}`} suppressHydrationWarning>
       <head>
-        {/* Google Analytics */}
-        {globalSettings?.google_analytics_id && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${globalSettings?.google_analytics_id}`}
-              strategy="beforeInteractive"
-            />
-            <Script id="ga-init" strategy="beforeInteractive">
-              {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${globalSettings?.google_analytics_id}');`}
-            </Script>
-          </>
-        )}
-
-        {/* Google Tag Manager */}
-        {globalSettings?.google_tag_manager && (
-          <Script id="gtm-init" strategy="beforeInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${globalSettings?.google_tag_manager}');`}
-          </Script>
-        )}
-
         {/* Schema Markup / JSON-LD */}
         {(globalSettings?.json_ld || globalSettings?.schema_markup) && (
           <script
@@ -107,7 +87,27 @@ export default async function RootLayout({
           />
         )}
       </head>
-      <body className="antialiased">
+      <body className="antialiased" suppressHydrationWarning>
+        {/* Google Analytics — afterInteractive avoids beforeInteractive head hydration races */}
+        {globalSettings?.google_analytics_id && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${globalSettings.google_analytics_id}`}
+              strategy="afterInteractive"
+            />
+            <Script id="ga-init" strategy="afterInteractive">
+              {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${globalSettings.google_analytics_id}');`}
+            </Script>
+          </>
+        )}
+
+        {/* Google Tag Manager */}
+        {globalSettings?.google_tag_manager && (
+          <Script id="gtm-init" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${globalSettings.google_tag_manager}');`}
+          </Script>
+        )}
+
         {process.env.NEXT_PUBLIC_ENABLE_CONTENT_PROTECTION !== "false" && (
           <ContentProtection />
         )}
